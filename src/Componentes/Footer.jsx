1c4c3ece8335c3206c0e3a8e04d17cf9db0e6da8
Akimithsu <rmamanisuc@unsa.edu.pe>
2023-05-03 13:19:40 -0500

    primer avance footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,8 +1,219 @@
+import React from "react";
+import "../Estilos/Rodrigo.css"
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <MDBFooter className="text-center" color="white" bgColor="dark">
+      <MDBContainer className="p-4">
+        <section className="">
+          <MDBRow>
+            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+              <div class="logo-footer">
+                <img src="../src/assets/logo.png" alt="Icon-Logo" />
+              </div>
+              <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                id="footer-space-line"
+              />
+              <p>
+                <i class="fas fa-home mr-3"></i> 27 Division St, New York,
+                United State America
+              </p>
+              <p>
+                <i class="fas fa-phone mr-3"></i> +1 (044) 123 456 789
+              </p>
+              <p>
+                <i class="fas fa-envelope mr-3"></i> info@example.com
+              </p>
+            </MDBCol>
+
+            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+              <h6 class="text-uppercase fw-bold">Useful links</h6>
+              <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                id="footer-space-line"
+              />
+              <ul>
+                <li>
+                  <p>
+                    <a href="#!" class="text-white">
+                      About us
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <a href="#!" class="text-white">
+                      About Us V2
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <a href="#!" class="text-white">
+                      Contact Us
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <a href="#!" class="text-white">
+                      Our Blog
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <a href="#!" class="text-white">
+                      Shop
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    <a href="#!" class="text-white">
+                      Testimonial
+                    </a>
+                  </p>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+
+
+              <h6 class="text-uppercase fw-bold">Our Service</h6>
+              <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                id="footer-space-line"
+              />
+              <p>
+                <a href="#!" class="text-white">
+                  Workout
+                </a>
+              </p>
+              <p>
+                <a href="#!" class="text-white">
+                  Progression
+                </a>
+              </p>
+              <p>
+                <a href="#!" class="text-white">
+                  Nutrition
+                </a>
+              </p>
+              <p>
+                <a href="#!" class="text-white">
+                  Workout
+                </a>
+              </p>
+              <p>
+                <a href="#!" class="text-white">
+                  Progression
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+              <h6 class="text-uppercase fw-bold">Free Consultation</h6>
+              <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                id="footer-space-line"
+              />
+              <p class="">
+                Power GYM Lift is champ in providing its users with absolutely
+                everything a fitness or gym site needs
+              </p>
+              <section className="mb-4">
+                <MDBBtn
+                  outline
+                  color="light"
+                  floating
+                  className="m-1"
+                  href="#!"
+                  role="button"
+                >
+                  <MDBIcon fab icon="facebook-f" />
+                </MDBBtn>
+
+                <MDBBtn
+                  outline
+                  color="light"
+                  floating
+                  className="m-1"
+                  href="#!"
+                  role="button"
+                >
+                  <MDBIcon fab icon="twitter" />
+                </MDBBtn>
+
+                <MDBBtn
+                  outline
+                  color="light"
+                  floating
+                  className="m-1"
+                  href="#!"
+                  role="button"
+                >
+                  <MDBIcon fab icon="google" />
+                </MDBBtn>
+
+                <MDBBtn
+                  outline
+                  color="light"
+                  floating
+                  className="m-1"
+                  href="#!"
+                  role="button"
+                >
+                  <MDBIcon fab icon="instagram" />
+                </MDBBtn>
+
+                <MDBBtn
+                  outline
+                  color="light"
+                  floating
+                  className="m-1"
+                  href="#!"
+                  role="button"
+                >
+                  <MDBIcon fab icon="linkedin-in" />
+                </MDBBtn>
+
+                <MDBBtn
+                  outline
+                  color="light"
+                  floating
+                  className="m-1"
+                  href="#!"
+                  role="button"
+                >
+                  <MDBIcon fab icon="github" />
+                </MDBBtn>
+              </section>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
+
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2020 Copyright:
+        
+      </div>
+    </MDBFooter>
+  );
 }
 
-export default Footer
+export default Footer;
